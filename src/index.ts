2615986878f353c100b4aa882d6322396152bab4
Tristan Simon <simontristan7766@gmail.com>
2024-07-19 10:45:54 +0100

    Checkpoint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("hi")
+import * as Discord from 'discord.js'
+import * as env from 'dotenv'
+
+const Client = new Discord.Client({
+    intents: [3276799]
+})
+
+Client.login(process.env.TOKEN)
