5b2c49ed2444d9ed413a1f98418b8d758a63eed1
Tristan Simon <simontristan7766@gmail.com>
2024-07-23 06:24:04 +0200

    made events

--- src/MessageReply.ts
+++ src/MessageReply.ts
@@ -1,6 +1,21 @@
-import * as Discord from 'discord.js'
+import * as Discord from "discord.js";
+import * as Settings from "../.vscode/Settings.json";
+const prefix = Settings.Prefix;
+const commands = Settings.Commands;
 
 export function MessageReply(message: Discord.Message) {
     if (message.author.bot) return;
+    const messagecontent = message.content;
+    const args = messagecontent.split(" ");
 
+    if (args[0] == prefix.HelpPrefix + commands.help && args[1] === "test") {
+        message.reply("you used the test command");
+    }
+
+    if (messagecontent == prefix.HelpPrefix + commands.help) {
+        message.reply("you used the help command");
+    }
+
+    
+        
 }
