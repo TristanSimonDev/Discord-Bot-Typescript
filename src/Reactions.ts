d2f86d64fef77173934c517486fd6418d1a3b8e6
Tristan | S. <simontristandev@gmail.com>
2024-08-11 12:34:41 +0200

    done with reactions for now

--- src/Reactions.ts
+++ src/Reactions.ts
@@ -1,11 +1,23 @@
 import * as Discord from "discord.js";
+import * as SettingsJSON from '@vscode/Settings.json'
+import { resolve } from "path";
+
+function delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
 
 export async function reaction([reaction, user]: [
     Discord.MessageReaction | Discord.PartialMessageReaction,
     Discord.User | Discord.PartialUser
 ]) {
-    const SelfRoleEmoji = ["🟢", "🟡", "🔥"];
+    if (reaction.message.author?.bot) return;
+    const ReactionRoleLogChannel = reaction.message.guild?.channels.cache.get(SettingsJSON.Channels["ReactionRoleLog-Channel"]) as Discord.TextChannel
+
+
+    const SelfRoleEmoji = Object.values(SettingsJSON.Roles.ReactionRoleEmoji);
+
     if (SelfRoleEmoji.includes(reaction.emoji.name as string)) {
         console.log(reaction.emoji.name);
+        const reactionmessage = await ReactionRoleLogChannel.send(`User: ${reaction.message.author?.globalName}, with Reaction ${reaction.emoji.name}`)        
     }
 }
--- src/Update/FetchOnStart.ts
+++ src/Update/FetchOnStart.ts
@@ -1,20 +1,25 @@
 import * as Discord from "discord.js";
 import * as SettingsJSON from "@vscode/Settings.json";
 
+const emojis = Object.values(SettingsJSON.Roles.ReactionRoleEmoji)
 export async function Init(Client: Discord.Client) {
+    
     const FetchReactionInRolesCH = async ([MessageID, ChannelId]: [string, string]) => { 
         const channel = Client.channels.cache.get(ChannelId) as Discord.TextChannel;
         const message = await channel.messages.fetch(MessageID);
 
         const reactions = message.reactions.cache;
 
-        reactions.forEach(reaction => {
+        reactions.forEach (async reaction => {
             console.log(`Reaction: ${reaction.emoji.name}, CNT ${reaction.count}`)
         });
+        for (const emoji of emojis) {
+            await message.react(emoji);
+        }
     };
 
     //Run Async
-    try {
+    try  {
         FetchReactionInRolesCH([SettingsJSON.Messages["ReactionRole-Message"], SettingsJSON.Channels["ReactionRole-Channle"]]);
     } catch (error) {
         console.error(error);     
